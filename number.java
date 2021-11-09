0f6045c70df7b7e57ebc283935e077a62f9e69c4
tsaizhungyou <93572214+tsaizhungyou@users.noreply.github.com>
2021-11-09 16:33:43 +0800

    Add files via upload

---
+++ number.java
@@ -0,0 +1,8 @@
+public class number {
+    public static void main(String[] args){
+        double height = 1.83;
+        float weight = 65f;
+        System.out.println(weight/height);
+
+    }
+}
