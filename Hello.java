28727eaf611d24dc50da591d5f35da4dbd365cc9
LinJie09 <zxc664231@gmail.com>
2021-12-21 00:18:04 +0800

    Changed

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
 
-        System.out.println("Hellooooooooo");//firstllll
+        System.out.println("Hellooooooooo");//firstll
 
         System.out.println("FUCKTHEWORD");
         System.out.println("FUCKyouevereyone");
